343299fae13134fbbaeda4a20e02dcd797eacb6d
Brandon Retana <branrecha140356@gmail.com>
2023-05-20 10:53:22 -0600

    changed names

--- Prolog/Project.pl
+++ Prolog/Project.pl
@@ -1,9 +1,4 @@
-
-%                                                   TAREA PROGRAMADA III
-
-% -------------------------------------------------------------------------------------------
 % DEFINICION DE HECHOS
-% -------------------------------------------------------------------------------------------
 conection(a,b,10).
 conection(b,c,3).
 conection(b,e,5).
@@ -42,9 +37,8 @@ percentage(f, h, 0.5).
 percentage(i, h, 3.0).
 percentage(_ , [], 0.0).
 
-% -------------------------------------------------------------------------------------------
+
 % FUNCION RECURSIVA CAMINO QUE VA CONECTANDO LOS NODOS
-% -------------------------------------------------------------------------------------------
 camino(A, B, [A, B], X) :-
     conection(A, B, X).
 camino(A, B, CaminoAB, Largo) :-
@@ -54,23 +48,19 @@ camino(A, B, CaminoAB, Largo) :-
     Largo is X + LargoCB.
 
 
-% -------------------------------------------------------------------------------------------
 % FUNCION PRINCIPAL QUE SE LLAMA EN LA INTERFAZ, ORDENA EL CAMINO Y LO IMPRIME AL USUARIO, LLAMA A LA FUNCION CAMINO
-% -------------------------------------------------------------------------------------------
-caminoMasCorto(A, B) :-
+shortestPath(A, B) :-
     findall(p(Costo, Camino),
             camino(A, B, Camino, Costo),
             Caminos),
     sort(Caminos, Lista),
     Lista = [p(Costo1, Camino1) | _],
-    printCamino(Camino1),
+    printPath(Camino1),
     printpercentage(Camino1, Comision1),
     writef(' (con distancia de %d km y una comision de %d%)\n', [Costo1, Comision1]).
 
 
-% -------------------------------------------------------------------------------------------
 % FUNCION RECURSIVA QUE RECORRE LA LISTA DEL CAMINO SOLUCION E IMPRIME EL COSTO EN percentage
-% -------------------------------------------------------------------------------------------
 printpercentage([], 0).
 printpercentage([X|T], Y) :-
 	car(T, Cabeza),
@@ -78,20 +68,17 @@ printpercentage([X|T], Y) :-
     printpercentage(T, C),
     Y is Comision + C.
 
-% -------------------------------------------------------------------------------------------
+
 % EXTRAE LA CABEZA DE UNA LISTA, SE USA EN PRINT percentage
-% -------------------------------------------------------------------------------------------
 car([], []).
 car([X|Y], X).
 
-% -------------------------------------------------------------------------------------------
 % FUNCION RECURSIVA QUE RECORRE LA LISTA DE SOLUCION E IMPRIME LA SOLUCION
-% -------------------------------------------------------------------------------------------
-printCamino([]).
-printCamino([X]) :-
+printPath([]).
+printPath([X]) :-
     !, write(X).
-printCamino([X|T]) :-
+printPath([X|T]) :-
     write(X),
     write(', '),
-    printCamino(T).
+    printPath(T).
 
